603a496520247559836785bed282e362e9aaca60
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-04-07 14:00:53 -0400

    4/7

---
+++ app/src/views/SignLogInView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Do you want to sign up or log in?</h1>
+    <button>Sign Up</button>
+    <button>Log In</button>
+  </main>
+</template>
