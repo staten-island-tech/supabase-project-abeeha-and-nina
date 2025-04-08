daf26b07b0542cdc6caa1811d8c18537e7613b36
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-04-08 13:35:13 -0400

    4/8

---
+++ app/src/components/auth/RegisterForm.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Register here!</h1>
+        <h2>What is your name?</h2>
+        <h2></h2>
+    </div>
+</template>
+
+<script setup>
+
+</script>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <h1>Do you want to sign up or log in?</h1>
+    <RouterLink to="/register" class="text-blue-600 hover:underline font-medium"
+        >Sign Up</RouterLink>
+        <RouterLink to="/login" class="text-blue-600 hover:underline font-medium"
+        >Sign In</RouterLink>
+
 </template>
