b600d8da4c9a04019e8ce3c4b21e443944e88eb9
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-04-08 13:52:34 -0400

    register

--- app/src/components/auth/RegisterForm.vue
+++ app/src/components/auth/RegisterForm.vue
@@ -1,8 +1,15 @@
 <template>
     <div>
         <h1>Register here!</h1>
-        <h2>What is your name?</h2>
-        <h2></h2>
+        <form>
+            <label for="name">What is your name?</label><br>
+            <input type="text" id="name" name="name"><br>
+            <label for="email">What is your email?</label><br>
+            <input type="text" id="email" name="email">
+            <label for="password">Create a secure password:</label>
+            <input type="password" id="password" name="password">
+            <input type="submit">
+        </form>
     </div>
 </template>
 
