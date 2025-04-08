e9051ed5af1cc707b8319baf27056b93f4ab0edc
Abeeha-Z <abeehaz@nycstudents.net>
2025-04-08 14:01:20 -0400

    4/8

--- app/src/supabase.ts
+++ app/src/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
+
 const supabase = createClient('<https://xycrjhhgpeqxbwydtjzl.supabase.co>', '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5Y3JqaGhncGVxeGJ3eWR0anpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2MTY3NjAsImV4cCI6MjA1OTE5Mjc2MH0.Jk3Zu-0l_43pJ-aI5djyy2Ot5GeIRnxy4WPJSJrsI9M>')
 const App = () => <Auth supabaseClient={supabase} />
