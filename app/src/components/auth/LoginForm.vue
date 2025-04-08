ab4fa8d36b41a6e30679b1b2c2b1d2c4d09e2b0a
Abeeha-Z <abeehaz@nycstudents.net>
2025-04-08 13:53:32 -0400

    4/8-login

---
+++ app/src/components/auth/LoginForm.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import supabase from supabase.ts
+
+async function signInWithEmail() {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: 'user.email',
+    password: '//user.password',
+  })
+}
+</script>
+
+<template>
+<h1>Login</h1>
+<input placeholder="Email" />
+<input placeholder="Password" />
+<btn type="submit" @click="signInWithEmail()">Submit</btn>
+
+
+</template>
