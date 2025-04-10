7427248831703b225ddf32ad9ee5e97b498267d0
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-04-09 20:29:09 -0400

    4/9

--- app/src/components/auth/RegisterForm.vue
+++ app/src/components/auth/RegisterForm.vue
@@ -1,18 +1,41 @@
 <template>
     <div>
-        <h1>Register here!</h1>
-        <form>
-            <label for="name">What is your name?</label><br>
-            <input type="text" id="name" name="name"><br>
-            <label for="email">What is your email?</label><br>
-            <input type="text" id="email" name="email"><br>
-            <label for="password">Create a secure password:</label>
-            <input type="password" id="password" name="password"><br>
-            <input type="submit">
-        </form>
+      <h1>Register here!</h1>
+      <form @submit.prevent="registerUser">
+        <label for="name">What is your name?</label><br>
+        <input type="text" id="name" v-model="name"><br>
+        <label for="email">What is your email?</label><br>
+        <input type="email" id="email" v-model="email"><br>
+        <label for="password">Create a secure password:</label><br>
+        <input type="password" id="password" v-model="password"><br>
+        <input type="submit" value="Register for an account">
+      </form>
     </div>
-</template>
+  </template>
 
 <script setup>
+import { ref } from 'vue'
+import {supabase} from "../supabase.ts"
 
+const name = ref('')
+const email = ref('')
+const password = ref('')
+
+const registerUser = async () => {
+  const { data, error } = await supabase.auth.signUp({
+    email: email.value,
+    password: password.value,
+    options: {
+      data: {
+        name: name.value
+      }
+    }
+  })
+
+  if (error) {
+    alert('error:', error.message)
+  } else {
+    alert('You are now registered in the Finance App!', data)
+  }
+}
 </script>
