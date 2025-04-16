60843f2aa205630d8da188baa3b6652cca2dd7aa
Abeeha-Z <abeehaz@nycstudents.net>
2025-04-16 15:58:08 -0400

    fv

---
+++ app/src/views/AuthPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Do you want to sign up or log in?</h1>
+    <RouterLink to="/register" class="text-blue-600 hover:underline font-medium"
+        >Sign Up</RouterLink>
+        <RouterLink to="/login" class="text-blue-600 hover:underline font-medium"
+        >Sign In</RouterLink>
+  </div>
+</template>
+
+<style>
+
+</style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,10 +3,6 @@
 </script>
 
 <template>
-    <h1>Do you want to sign up or log in?</h1>
-    <RouterLink to="/register" class="text-blue-600 hover:underline font-medium"
-        >Sign Up</RouterLink>
-        <RouterLink to="/login" class="text-blue-600 hover:underline font-medium"
-        >Sign In</RouterLink>
+
 
 </template>
