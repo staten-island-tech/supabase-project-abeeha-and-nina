0542186a7660909db58dfaa6f822a0c2dea4c7fa
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-05-07 13:20:59 -0400

    5/6

--- app/src/components/auth/LoginForm.vue
+++ app/src/components/auth/LoginForm.vue
@@ -16,12 +16,17 @@ async function signInWithEmail() {
 </script>
 
 <template>
-<form>
-  <h1>Login</h1>
-  <input placeholder="Email" type="email" id="email" v-model="email"><br>
-  <input placeholder="Password" type="password" id="password" v-model="password"><br>
-  <input type="submit" @click="signInWithEmail()"></input>
-</form>
-<RouterLink to="/reset" class="text-blue-600 hover:underline font-medium"
-        >Reset Password</RouterLink><br>
+<div class="flex justify-center">
+  <div class="bg-base-100 w-1/4 pb-8 pt-6 h-auto border-2 hover:border-4 duration-200 rounded-lg my-5 min-w-64 shadow-lg">
+    <form>
+      <h1 class="text-center text-2xl text-neutral my-1 mx-5 font-bold">Login here!</h1>
+      <label for="email" class="flex justify-center pt-3">What is your email?</label>
+      <input placeholder="Email" type="email" id="email" v-model="email"><br>
+      <input placeholder="Password" type="password" id="password" v-model="password"><br>
+      <input type="submit" @click="signInWithEmail()"></input>
+    </form>
+    <RouterLink to="/reset" class="text-blue-600 hover:underline font-medium"
+            >Reset Password</RouterLink><br>
+  </div>
+</div>
 </template>
--- app/src/views/AuthPage.vue
+++ app/src/views/AuthPage.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex justify-center">
-  <div class="bg-base-100 w-1/5 pb-8 pt-6 h-auto border-2 hover:border-4 rounded-lg my-5 min-w-64 shadow-lg">
-    <h1 class="text-center text-2xl text-neutral my-1 mx-5 font-bold">Do you want to sign up or log in?</h1>
-    <RouterLink to="/register" class="text-blue-600 hover:underline font-medium text-center mb-2 italic"
+  <div class="bg-base-100 w-1/4 pb-8 pt-6 h-auto border-2 hover:border-4 duration-200 rounded-lg my-5 min-w-64 shadow-lg">
+    <h1 class="text-center text-2xl text-neutral my-1 mx-5 font-bold">Would you like to sign up or log in?</h1>
+    <RouterLink to="/register" class="hover:text-xl duration-200 pt-8 font-medium flex justify-center mb-2 mx-5"
       >Sign Up</RouterLink
     ><br/>
-    <RouterLink to="/login" class="text-blue-600 hover:underline font-medium text-center mb-2 mx-5">Sign In</RouterLink>
+    <RouterLink to="/login" class="hover:text-xl duration-200 font-medium flex justify-center mb-2 mx-5">Sign In</RouterLink>
   </div>
 </div>
 </template>
