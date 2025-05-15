d66a45fe2794d1ef5856c5ee9f691c15ca252283
Abeeha-Z <abeehaz@nycstudents.net>
2025-05-15 09:27:06 -0400

    5/15

--- app/src/components/QuestionForm.vue
+++ app/src/components/QuestionForm.vue
@@ -1,9 +1,10 @@
 <template>
 
 <form>
-    <h1>INCOME FORM</h1>
-    <input id="p_income" placeholder="Enter Monthly Income">
+    <h1>INCOME & SAVINGS FORM</h1>
+    <input id="p_income" placeholder="Enter Estimated Monthly Income">
     <input id="s_income" placeholder="Enter Secondary Income">
+
 
 
 </form>
@@ -11,41 +12,71 @@
 <form>
     <h1>EXPENSE FORM</h1>
     <div>
-    <input id="groceries" placeholder="Estimated Monthly Grocery cost">
-
-    <select v-model="selected">
-    <option v-for="groc_option in groc_options" :value="groc_option.value">
-        {{ groc_option.text }}
-    </option>
+    <input v-model="expenseData.groceries" placeholder="Estimated Monthly Grocery cost"/>
+    <select v-model="expenseData.groceries.cost_type" >
+        <option value="flex">Flexible</option>
+        <option value="fixed">Fixed</option>
     </select>
     </div>
+
     <div>
-        <input id="utilities" placeholder="Estimated Monthly Utilities cost">
-
-
+    <input v-model="expenseData.utilities" placeholder="Estimated Monthly Utilities cost"/>
+    <select v-model="expenseData.utilities.cost_type" >
+        <option value="flex">Flexible</option>
+        <option value="fixed">Fixed</option>
+    </select>
     </div>
-   
 
+    <div>
+    <input id="expenseData.shopping" placeholder="Estimated Monthly Shopping cost"/>
+    <select v-model="expenseData.shopping.cost_type" >
+        <option value="flex">Flexible</option>
+        <option value="fixed">Fixed</option>
+    </select>
+    </div>
 
+    <button type="submit" @click="previewInfo()">Submit Form</button>
 </form>
+
+<div>
+    <h1>Please verify information before submitting.</h1><br>
+
+    <h2>Monthly Gorcery Cost: {{expenseData.groceries.value}} </h2>
+    <h2>Type: {{ expenseData.groceries.cost_type }}</h2> <br>
+
+    <h2>Monthly Utilities Cost: {{expenseData.utilities.value}}</h2>
+    <h2>Type: {{ expenseData.utilities.cost_type }}</h2><br>
+
+    <h2>Monthly Shopping Cost: {{expenseData.shopping.value}}</h2>
+    <h2>Type: {{ expenseData.shopping.cost_type }}</h2><br>
+
+
+    
+
+</div>
 
 </template>
 
 
 <script setup lang="ts">
-import {ref} from "vue"
+import {reactive, ref} from "vue"
 
-const selected = ref('flex')
 
-const groc_options = ref([
-  { text: 'Flexible', value: 'flexible' },
-  { text: 'Rigid', value: 'rigid' },
-])
+const expenseData = reactive({
+    groceries: { value:0, cost_type:""},  //in types.ts, interface formData, int expected for value, and string for cost_type
+    utilities: { value:0, cost_type:""},
+    shopping: {value:0, cost_type:""}
 
-function appendInfo() {
-    const groc_cost = 
+
+
+})
+
+function previewInfo() {
+    
 }
+    
 
-//append info to profile, 
+//append info to formData before profile, bc we need to likely preview it before changing main state
+//user needs to be able to select type, type needs to be shown. So each property has its own property so it needs to be an object
 
 </script>
