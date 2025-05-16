014e58a68eef3c9815d3ee8d08a4bd969243d2c1
Abeeha-Z <abeehaz@nycstudents.net>
2025-05-15 21:01:42 -0400

    5/15

--- app/src/components/QuestionForm.vue
+++ app/src/components/QuestionForm.vue
@@ -12,49 +12,43 @@
 <form>
     <h1>EXPENSE FORM</h1>
     <div>
-    <input v-model="expenseData.groceries" placeholder="Estimated Monthly Grocery cost"/>
+    <input v-model="expenseData.groceries.value" placeholder="Estimated Monthly Grocery cost"/>
     <select v-model="expenseData.groceries.cost_type" >
         <option value="flex">Flexible</option>
         <option value="fixed">Fixed</option>
     </select>
     </div>
 
-    <div>
+    <!-- <div>
     <input v-model="expenseData.utilities" placeholder="Estimated Monthly Utilities cost"/>
     <select v-model="expenseData.utilities.cost_type" >
         <option value="flex">Flexible</option>
         <option value="fixed">Fixed</option>
     </select>
     </div>
-
+    <h1>SHOPPING</h1>
     <div>
     <input id="expenseData.shopping" placeholder="Estimated Monthly Shopping cost"/>
     <select v-model="expenseData.shopping.cost_type" >
         <option value="flexible">Flexible</option>
         <option value="fixed">Fixed</option>
     </select>
-    </div>
+    </div> -->
 
     <button type="submit" @click="previewInfo()">Submit Form</button>
 </form>
 
-<div>
-    <h1>Please verify that the information provided is:</h1><br>
+<div :v-if="{showPreview}">
+    <h1>Please verify that the information provided is correct:</h1><br>
 
-    <h2>Monthly Gorcery Cost: {{expenseData.groceries.value}} </h2>
-    <h2>Type: {{ expenseData.groceries.cost_type }}</h2> <br>
+    <h2 v-for="(item,name) in expenseData" :key="name">{{ item.name }} - Est. Monthly Cost: {{item.value }}, Cost Type: {{ item.cost_type }} </h2>
 
-    <h2>Monthly Utilities Cost: {{expenseData.utilities.value}}</h2>
-    <h2>Type: {{ expenseData.utilities.cost_type }}</h2><br>
-
-    <h2>Monthly Shopping Cost: {{expenseData.shopping.value}}</h2>
-    <h2>Type: {{ expenseData.shopping.cost_type }}</h2><br>
 </div>
 
 </template>
 
 <script setup lang="ts">
-import UserProfile from "./UserProfile.vue";
+
 import {reactive, ref} from "vue"
 
 
@@ -64,10 +58,26 @@ const expenseData = reactive({
     shopping: {value:0, cost_type:"fixed"}
 
 })
+//submit pressed, displayprev = true, it displays, v-else on form and it dissappears
+//button in preview has option to submit(appends data to profile) or edit, which turns the preview off and the form on.
+//repeat with the submit button
+
+const showPreview = ref("false")
 
 function previewInfo() {
+    showPreview.value = "true"
+
+
 
 }
+
+//Expenses: Category 1, 2, etc(class). Individual Costs: properties =>
+// Select: Monthly or Weekly, Type: Flexible or Fixed, Value/Cost, Weekly/Monthly Spending Goal: 
+
+//Income
+//Add Primary Income: 
+//Savings Goal per week/month
+
 
 
 
