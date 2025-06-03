0a06507d3c62e5b76acbb090e1643385bd4bdbb1
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-06-03 13:59:45 -0400

    6/3

--- app/src/views/DashboardView.vue
+++ app/src/views/DashboardView.vue
@@ -1,6 +1,10 @@
 <template>
-    <RouterLink to="/questionform">Click Here to Build My Profile!</RouterLink>
-    
+    <div class="hover:text-xl duration-150 font-medium italic px-3 py-3 rounded-lg bg-base-200">
+        <RouterLink to="/questionform">Click Here to Build My Profile!</RouterLink>
+    </div> <br/>
+    <div class="hover:text-xl duration-150 font-medium italic px-3 py-3 rounded-lg bg-base-200">
+        <RouterLink to="/expenseform">Click Here to Organize My Expenses!</RouterLink>
+    </div>
 </template>
 
 <script setup lang="ts">
